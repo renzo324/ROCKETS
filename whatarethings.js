ef0e4ec17dfc8e212bd762e20b198d3de6c4347d
renzo324 <31312773+renzo324@users.noreply.github.com>
2017-09-18 14:25:43 +0800

    lessons on variables and fucntions

--- whatarethings.js
+++ whatarethings.js
@@ -1,1 +1,29 @@
 // this explains what java can do
+// Camel case: favColor ( upper cases in teh middle words but not the first word)
+// var = variable
+var favColor = 'red';
+// can use ' ' or " "
+// arrays: 	basically what lists are in PY but in JS
+var myFavColors = ['red', 'gold', 'black', 'purple'];
+var numOfFavColors = 4;
+// you can into true or false values  in java
+var hadGotFavColors = true;
+//objects lists that can contain lists and other variables such as strings. 
+//contained in curly braces
+var archObject = {
+	firstName: 'Lorenzo',
+	lastName: 'Ibay',
+	favColors: ['red', 'gold', 'black', 'purple'],
+	age: 21,
+	isMale: true,
+
+};
+// variables in an object can be brought up with the dotNotation
+//functions are things that can be reused. sort of like functions in Python
+var doSomething = function () {
+	console.log("IM DOING A THING!!");
+}
+var whatAreMyFavColors = function() {
+	return = 'red':
+	
+}
